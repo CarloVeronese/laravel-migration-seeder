ff43dbb65037469b70efb74e3f5fcd34cb062ac0
CarloVeronese <carlo.veronese97@gmail.com>
2023-12-13 16:01:32 +0100

    created migration

--- database/migrations/2023_12_13_145922_create_trains_table.php
+++ database/migrations/2023_12_13_145922_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime ('departure_time');
+            $table->dateTime ('arrival_time');
+            $table->string('train_id')->unique();
+            $table->tinyInteger('n_carriages')->unsigned();
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('is_cancelled')->default(0);
             $table->timestamps();
         });
     }
